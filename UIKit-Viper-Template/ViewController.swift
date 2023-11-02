b4f2ac7a6f0da0016123892dcdce3637ef75603b
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-11-03 00:25:05 +0200

    Initial Commit

---
+++ UIKit-Viper-Template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Viper-Template
+//
+//  Created by Telem Tobi on 03/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
